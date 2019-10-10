7a2310aec41c7cc7e7fd0ed7332520d9d7da5ba7
02TheBear <Fredrik.Berzins@gmail.com>
2019-10-10 14:21:39 +0200

    +old files test 2

---
+++ student/loggedin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Booking</title>
+    <link rel="stylesheet" href="background_S.css">
+  </head>
+  <body>
+    <main>
+      <form class="" action="../script/loggedin-script.php" method="post">
+        <input type="text" name="FName" placeholder="FirstName">
+        <input type="text" name="LName" placeholder="LastName">
+        <button type="submit" name="loggedin-submit">Login</button>
+      </form>
+    </main>
+  </body>
+  <footer>
+  </footer>
+</html>
---
+++ student/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Booking</title>
+    <link rel="stylesheet" href="login.css">
+  </head>
+  <body>
+    <main>
+      <form class="" action="../script/login-script.php" method="post">
+        <input type="text" name="UCode" placeholder="UserCode">
+        <button type="submit" name="login-submit">Login</button>
+      </form>
+    </main>
+  </body>
+  <footer>
+    <a href="/admin/login_A.php">
+      <p>To admin login</p>
+    </a>
+  </footer>

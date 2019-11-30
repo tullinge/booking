8499de779f6328f4720bb651a6607e82be326133
Vilhelm Prytz <vilhelm@prytznet.se>
2019-11-30 01:43:33 +0100

    Use code formatter 'black'

--- admin/code_generator.py
+++ admin/code_generator.py
@@ -1,30 +1,33 @@
 import random as r
 
 temporary_code_list = []
-finnished_code_list = ['BEAR', 'MINK', 'SEAL', 'KOKO', 'GRIS', 'HARE', 'JOEY']
+finnished_code_list = ["BEAR", "MINK", "SEAL", "KOKO", "GRIS", "HARE", "JOEY"]
+
 
 def main():
-    x = int(input('Hur många koder vill du ha?\n'))
-    if x < 36**4:
+    x = int(input("Hur många koder vill du ha?\n"))
+    if x < 36 ** 4:
         for i in range(x - 7):
             temporary_code_list.append(generate_uuid())
         for code in temporary_code_list:
             if not code in finnished_code_list:
                 finnished_code_list.append(code)
-        file = open('finnished_codes.txt', 'w')
+        file = open("finnished_codes.txt", "w")
         file.write(str(finnished_code_list))
         print(finnished_code_list)
     else:
-        print('404 error')
+        print("404 error")
+
 
 def generate_uuid():
-    random_string = ''
-    random_str_seq = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    random_string = ""
+    random_str_seq = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     uuid_format = [4]
     for n in uuid_format:
         for i in range(0, n):
             random_string += str(random_str_seq[r.randint(0, len(random_str_seq) - 1)])
     return random_string
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- setup_db.py
+++ setup_db.py
@@ -1,42 +1,46 @@
 import pymysql
 from student import dconfig
 
+
 def _create_conn():
     return pymysql.connect(
         host=dconfig.DB_Server,
         user=dconfig.DB_Username,
         password=dconfig.DB_Password,
-        db=dconfig.DB_Name
+        db=dconfig.DB_Name,
     )
+
 
 def create_user_table():
     conn = _create_conn()
 
-    create_sql = '''CREATE TABLE users (
+    create_sql = """CREATE TABLE users (
         id INT NOT NULL AUTO_INCREMENT,
         email VARCHAR(64),
         name VARCHAR(64),
         PRIMARY KEY (id)
-    )'''
+    )"""
 
-    insert_sql = 'INSERT INTO `users` (`email`, `name`) VALUES (%s, %s)'
+    insert_sql = "INSERT INTO `users` (`email`, `name`) VALUES (%s, %s)"
 
     try:
         with conn.cursor() as cursor:
             cursor.execute(create_sql, ())
-            cursor.execute(insert_sql, ('mike@tyson.com', 'Mike'))
+            cursor.execute(insert_sql, ("mike@tyson.com", "Mike"))
         conn.commit()
     finally:
         conn.close()
+
 
 def drop_user_table():
     conn = _create_conn()
     try:
         with conn.cursor() as cursor:
-            cursor.execute('DROP TABLE users', ())
+            cursor.execute("DROP TABLE users", ())
         conn.commit()
     finally:
         conn.close()
+
 
 if __name__ == "__main__":
     drop_user_table()

514d313156ad678bc8e05e84136dc54928f4d180
02TheBear <Fredrik.Berzins@gmail.com>
2019-12-08 21:58:25 +0100

    add to flask import

--- routes/admin.py
+++ routes/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 
 # blueprint init
 admin_routes = Blueprint(
--- routes/student.py
+++ routes/student.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect ,request
 
 # blueprint init
 student_routes = Blueprint(
